714a510b5b18e3ba9e26526cb17edeb08504e3a8
MC Simoes <mariacristina.simoes@gmail.com>
2018-11-02 18:25:17 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,7 +4,9 @@ def turn_count(board)
     if turns == "X" || turns =="O"
   counter += 1 
 end
+end
 returns counter
+end
 
 
 def current_player(board)
@@ -12,26 +14,6 @@ counter = turn_count(board)
 if counter % 2 == 0
     return "X"
   else 
-    return "O"
-  end
-end
-
-
-def turn_count(board)
-  turns = 0
-  board.each do |space|
-    if space == "X" || space == "O"
-      turns += 1
-    end
-  end
-  return turns
-end
-
-def current_player(board)
-  turns = turn_count(board)
-  if turns % 2 == 0
-    return "X"
-  else
     return "O"
   end
 end
