7c5276e6143f1f378bc434ae30ef64e7415acb9e
MC Simoes <mariacristina.simoes@gmail.com>
2018-11-02 18:13:42 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,10 @@
-board =[" "]
+board = [" "," "," ", " "," ", " "," ", " "," "]
 def turn_count(board)
   counter = 0 
-  board.each.do|turns|
+  board.each do |turns|
   counter += 1 
 end
 
-brothers = ["Tim", "Tom", "Jim"]
-counter = 1
-brothers.each do |brother|
-  puts "This is loop number #{counter}"
-  puts "Stop hitting yourself #{brother}!"
-  counter += 1
-end
 
 def current_player(board)
   if turn_count(board) % 2 
