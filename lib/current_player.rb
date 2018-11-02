7963d902bb4d55d7f9520a1ecfbf375455bde166
MC Simoes <mariacristina.simoes@gmail.com>
2018-11-02 18:08:12 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,12 @@
+def turn_count(board)
+  counter = 0 
+  board.each.do|turns|
+  counter += 1 
+end
+
+def current_player(board)
+  if turn_count(board) % 2 
+    return "X"
+  else 
+    return "O"
+  end
